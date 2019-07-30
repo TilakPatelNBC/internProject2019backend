29817527b5f0d3e84fdee9020190299fb9b66ad6
Tilak <tilak.patel@nbcuni.com>
2019-07-30 13:39:07 -0400

    Working API with Jolene things

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
-from flask import request, redirect
+from flask import Flask, request, redirect, render_template
+from bson.json_util import dumps
 import requests
 import os
+import json
+from pprint import pprint
+from pymongo import MongoClient
 from os.path import join, dirname
 from dotenv import load_dotenv
+client = MongoClient('localhost', 27017)
+db = client['main']
+
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
@@ -19,9 +25,9 @@ application = Flask(__name__)
 
 @app.route('/')
 def default():
-    return redirect('/404page')
+    return render_template('404page.html')
 
-@app.route('/videos')
+@app.route('/videos') #requires results query arg
 def videos():
     if 'results' in request.args:
         length = request.args.get('results')
@@ -29,12 +35,9 @@ def videos():
         req = requests.get(request_url)
         yt_response = req.json()
         yt_response['status'] = req.status_code
-        return yt_response
+        return yt_response, req.status_code
     else:
-        return {
-            'status': 400,
-            'message': 'Please supply the params'
-        }
+        return {'message': 'Please supply the params'}, 400
 
 @app.route('/latest-articles') #returns the latest articles filtered by the eyebrow
 def articles():
@@ -48,11 +51,34 @@ def articles():
         'Origin': 'https://qa-aws01webql.cnbc.com',
     }
 
-    data = '{"query":"{\\npage(path: \\"/us-top-news-and-analysis\\") {\\n    brand\\n    layout {\\n      columns {\\n        modules {\\n          data {\\n            ... on featuredBreaker {\\n              url\\n              type\\n              title: eyebrow\\n              sectionLabel\\n              shorterHeadline\\n              assets(count: 8, promoted: true) {\\n                id\\n                url\\n                datePublished\\n                title\\n                brand\\n                author {\\n                  name\\n                }\\n              }\\n            }\\n          }\\n        }\\n      }\\n    }\\n  }\\n}\\n"}'
-    
+    data = '{"query":"{\\npage(\\n path:\\"/us-top-news-and-analysis/\\") {\\n brand\\n layout {\\n   columns {\\n     span\\n     modules {\\n       data {\\n          ...on heroLedePlusThree {\\n  id\\n            datePublished\\n  assets {\\n    id\\n    description\\n    title\\n    headline\\n    shorterDescription\\n    url\\n    promoImage {\\n      url\\n    }\\n  }\\n}\\n       }\\n      \\n     }\\n   }\\n }\\n}\\n}"}'
     response = requests.post('https://qa-aws01webql.cnbc.com/graphql', headers=headers, data=data)
-    var = response.json()['data']['page']['layout'][9]['columns'][0]['modules'][0]['data']
-    var['status'] = response.status_code
-    return var
+    data = response.json()['data']['page']['layout'][3]['columns'][0]['modules'][0]
+    return data, 200
+    
+
+@app.route('/create-user', methods = ['POST']) #creates a user and returns the information about the user
+def create_user():
+    name = 'invalid' if ('name' not in request.args) else request.args.get('name')
+    email = 'invalid' if ('email' not in request.args) else request.args.get('email')
+    age = -1 if ('age' not in request.args) else request.args.get('age')
+    industry = 'invalid' if ('industry' not in request.args) else request.args.get('industry')
+    interests = 'invalid' if ('interests' not in request.args) else request.args.get('interests')
+    password = 'invalid' if ('password' not in request.args) else request.args.get('password')
+    if name == "invalid" or email == "invalid" or age <= 8 or industry == "invalid" or interests == "invalid" or password == "invalid":
+        return {'message': 'invalid parameters'}, 400
+    users = db.Users
+    user_id = users.insert_one({
+        'name': name,
+        'email': email,
+        'age': age,
+        'industry': industry,
+        'interests': interests,
+        'password': password,
+        'history': []
+    }).inserted_id
+    jss = json.loads(dumps(users.find_one({"_id": user_id})))
+    return jss, 200
+
 if __name__ == "__main__":
     app.run()
